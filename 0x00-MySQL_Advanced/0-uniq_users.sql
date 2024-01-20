0864a93647988049d71d9ec56834731f17663030
Your Name <you@example.com>
2024-01-20 21:22:49 +0000

    true

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,15 +1,11 @@
--- 0. We are all unique!
+-- Creates a table users with below attributes
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+-- If table exists, script will not fail, can be executed on any database
 
--- Create database if not exists
-CREATE DATABASE IF NOT EXISTS holberton;
-
--- Use the holberton database
-USE holberton;
-
--- Create users table
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+       id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+       email VARCHAR(255) NOT NULL UNIQUE,
+       name VARCHAR(255)
 );
-
